6975e73de5f9af2cf1716c91d4a1d79485351364
张家政 <thuzjz@gmail.com>
2014-07-07 21:01:26 +0800

    add

--- out.js
+++ out.js
@@ -7,8 +7,15 @@ $(function(){
 		
 		$(this).find('.blue-button').removeClass('blue-button');
 		$(this).find('img').width('200px');
+		$(this).find('img').attr('src','http://snow-ren.com/1.jpg');
 		$(this).find('.student').each(function(){
 			$(this).children('a').text('1073741824').css('color','red');
 		})
+		
+		$(this).find('img').mouseover(function(){
+			$(this).attr('src','http://learn.cic.tsinghua.edu.cn:80/b/mycourse/courseExtension/downloadFile/2013-2014-3-44100343-0/thumbnail');
+		}).mouseleave(function(){
+			$(this).attr('src','http://snow-ren.com/1.jpg');
+		});
   	});
 });
